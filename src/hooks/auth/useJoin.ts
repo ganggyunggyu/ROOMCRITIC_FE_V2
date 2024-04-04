86cfecb1f568103f37502df9a441f6cf92441287
강경규 <qwzx16@naver.com>
2024-04-04 13:58:27 +0900

    FEAT: Hooks와 API요청 로직 분리

--- src/hooks/auth/useJoin.ts
+++ src/hooks/auth/useJoin.ts
@@ -1,38 +1,10 @@
-import { useState } from 'react';
-import axiosConfig from '../../api/axiosConfig';
-import { useNavigate } from 'react-router-dom';
+import { useMutation } from '@tanstack/react-query';
+import { submitJoin } from '../../api/API';
 
-type TRequestUserInfo = {
-  email?: string;
-  password?: string;
-  displayName?: string;
-  phoneNumber?: string;
-};
-
-type TJoinHook = {
-  submitJoin: () => Promise<void>;
-  data: string;
-};
-
-const useJoin = (requestUserInfo: TRequestUserInfo): TJoinHook => {
-  const navigator = useNavigate();
-  const [data, setData] = useState<string>('');
-  const submitJoin = async () => {
-    try {
-      const result = await axiosConfig.post('/auth/join', {
-        email: requestUserInfo.email,
-        password: requestUserInfo.password,
-        displayName: requestUserInfo.displayName,
-        phoneNumber: requestUserInfo.phoneNumber,
-      });
-
-      if (result.data.message === '회원가입 성공') navigator('/login');
-      setData(result.data.message);
-    } catch (error) {
-      console.log('가입요청실패 : ', error);
-    }
-  };
-
-  return { submitJoin, data };
+const useJoin = () => {
+  return useMutation({
+    mutationKey: ['join'],
+    mutationFn: submitJoin,
+  });
 };
 export default useJoin;
--- src/hooks/content/useContentFetch.ts
+++ src/hooks/content/useContentFetch.ts
@@ -1,19 +1,12 @@
-import axiosConfig from '../../api/axiosConfig';
-
+import * as API from '../../api/API';
 import { useQuery } from '@tanstack/react-query';
 
-const fetchDetailContent = async (contentType: string, contentId: string) => {
-  try {
-    const result = await axiosConfig.get(`content/${contentType}/${contentId}`);
-    return result;
-  } catch (err) {
-    console.log(err);
-  }
-};
 const useContentFetch = (contentType: string, contentId: string) => {
+  const queryKey = ['detailContent', contentType, contentId];
+
   const detailContentQuery = useQuery({
-    queryKey: ['detailContent', contentType, contentId],
-    queryFn: () => fetchDetailContent(contentType, contentId),
+    queryKey,
+    queryFn: () => API.getDetailContent(contentType, contentId),
   });
 
   return {
