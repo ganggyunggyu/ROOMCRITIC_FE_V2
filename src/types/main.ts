33579b533f1b58d9d27008266a12cda1ab94a5f8
강경규 <qwzx16@naver.com>
2024-04-15 20:09:52 +0900

    FEAT: 타입 추가

--- src/types/main.ts
+++ src/types/main.ts
@@ -1,6 +1,40 @@
-export type TRequestUserInfo = {
+export type TJoinUserDTO = {
   email: string;
   password: string;
   displayName: string;
   phoneNumber: string;
+};
+export type TLoginUserDTO = {
+  email: string;
+  password: string;
+};
+export type TContent = {
+  adult: boolean;
+  backdrop_path: string;
+  content_type: string;
+  genre_ids: number[];
+  id: number;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  release_date: Date;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+  _id: string;
+};
+export type TReviewCreateDTO = {
+  userId: string;
+  userName: string;
+  lineReview: string;
+  longReview: string;
+  grade: number;
+  contentPosterImg: string;
+  contentBackdropImg: string;
+  contentName: string;
+  contentId: string;
+  contentType: string;
 };
