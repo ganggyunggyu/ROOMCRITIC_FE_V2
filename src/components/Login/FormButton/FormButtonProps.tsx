0ad4fc0526b065e7b0c58ff304596c51d88c5f55
강경규 <qwzx16@naver.com>
2024-04-15 21:11:22 +0900

    FIX: eslint 이슈 주석처리

---
+++ src/components/Login/FormButton/FormButtonProps.tsx
@@ -0,0 +1,12 @@
+// export const loginButtonProps = {
+//   label: '로그인',
+//   bg: activeLogin ? 'main' : 'disable',
+//   disabled: !activeLogin,
+//   onClick: submitLogin,
+// };
+
+// export const joinButtonProps = {
+//   label: '회원가입',
+//   bg: 'main',
+//   onClick: directJoin,
+// };
