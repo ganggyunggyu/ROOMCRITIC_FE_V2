3e194858784763208b668e4a243c0e8297c4ddd4
강경규 <qwzx16@naver.com>
2024-04-25 19:24:26 +0900

    FEAT: redux persist add

--- src/app/store/index.ts
+++ src/app/store/index.ts
@@ -7,13 +7,18 @@ import { persistReducer, persistStore } from 'redux-persist';
 const persistConfig = {
   key: 'root',
   storage,
-  whitelist: ['darkMode'],
+  whitelist: ['darkMode', 'search'],
+  // blacklist: ['darkModeClasses'], // 직렬화할 수 없는 값이므로 저장에서 제외
 };
 
 const PersistReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
   reducer: PersistReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
