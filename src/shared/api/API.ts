664e3aa847615813a14068a0639e4804b118875a
강경규 <qwzx16@naver.com>
2024-04-25 19:42:29 +0900

    FEAT: 좋아요, 보고싶어요, 프로필 불러오기 API, 훅 생성

--- src/shared/api/API.ts
+++ src/shared/api/API.ts
@@ -1,6 +1,4 @@
-import { Dispatch, SetStateAction } from 'react';
-import { TContent, TJoinUserDTO, TLoginUserDTO } from '../../app/types/main';
-
+import { TJoinUserDTO, TLoginUserDTO } from '../../app/types/main';
 import AxiosConfig from '../../app/config/axios-config';
 import { ReviewCreateDTO } from '../../app/types/dtos';
 
@@ -9,7 +7,6 @@ export const submitJoin = async (joinUserDTO: TJoinUserDTO) => {
     const result = await AxiosConfig.post('/auth/join', {
       ...joinUserDTO,
     });
-
     return result;
   } catch (error) {
     console.debug(error);
@@ -29,12 +26,13 @@ export const submitLogin = async (loginUserDTO: TLoginUserDTO) => {
 export const fetchLoginStatus = async () => {
   try {
     const result = await AxiosConfig.get('/auth/login/check');
-    console.debug(result);
     if (result.status === 200) {
       return result;
     }
     if (result.status === 201) {
-      throw Error('세션 만료');
+      // throw Error('세션 만료');
+      // return '세션 만료';
+      return result;
     }
   } catch (error) {
     console.error('fetchLoginERROR !!', error);
@@ -50,37 +48,23 @@ export const getDetailContent = async (contentType: string, contentId: string) =
   }
 };
 
-export const fetchSearchContents = async (
-  searchValue: string,
-  setSearchContents: Dispatch<SetStateAction<TContent[]>>,
-) => {
+export const fetchSearchContents = async (searchValue: string) => {
   if (!searchValue) throw new Error('검색어 입력 필요');
   const result = await AxiosConfig.get(`content/search?search_value=${searchValue}`);
-  const contents: TContent[] = result.data.contents;
-  setSearchContents(contents);
+
   return result;
 };
 
-export const fetchSearchTvContents = async (
-  searchValue: string,
-  setSearchContents: Dispatch<SetStateAction<TContent[]>>,
-) => {
+export const fetchSearchTvContents = async (searchValue: string) => {
   if (!searchValue) throw new Error('검색어 입력 필요');
   const result = await AxiosConfig.get(`content/search/tv?search_value=${searchValue}`);
-  const contents: TContent[] = result.data.contents;
-  setSearchContents(contents);
 
   return result;
 };
-export const fetchSearchMovieContents = async (
-  searchValue: string,
-  setSearchContents: Dispatch<SetStateAction<TContent[]>>,
-) => {
+export const fetchSearchMovieContents = async (searchValue: string) => {
   if (!searchValue) throw new Error('검색어 입력 필요');
   const result = await AxiosConfig.get(`content/search/movie?search_value=${searchValue}`);
-  const contents: TContent[] = result.data.contents;
-  setSearchContents(contents);
-  console.table(contents);
+
   return result;
 };
 export const reviewCreate = async (reviewCreateDTO: ReviewCreateDTO) => {
@@ -120,4 +104,120 @@ export const fetchOwnerPickMovieContent = async () => {
 export const fetchTopRatedMovie = async () => {
   const result = await AxiosConfig.get('movie/top-rated-movies');
   return result;
+};
+export type AddWishContentRequestDTO = {
+  userId: string;
+  contentId: string;
+};
+export type AddWatchContentRequestDTO = {
+  userId: string;
+  contentId: string;
+};
+export type FetchWishContentRequestDTO = {
+  userId: string;
+  contentId: string;
+};
+export type FetchWatchContentRequestDTO = {
+  userId: string;
+  contentId: string;
+};
+export type FetchLikeReviewRequestDTO = {
+  userId: string;
+  reviewId: string;
+};
+export type SendLikeReviewRequestDTO = {
+  userId: string;
+  reviewId: string;
+};
+export type SendDislikeReviewRequestDTO = {
+  userId: string;
+  reviewId: string;
+};
+
+export const addWishContent = async (addWishContentRequestDTO: AddWishContentRequestDTO) => {
+  try {
+    const result = await AxiosConfig.post('contetn/wish', addWishContentRequestDTO);
+    return result;
+  } catch (error) {
+    console.error(error);
+    throw Error(error);
+  }
+};
+
+export const addWatchContent = async (addWatchContentRequestDTO: AddWatchContentRequestDTO) => {
+  try {
+    const result = await AxiosConfig.post('content/watch', addWatchContentRequestDTO);
+    return result;
+  } catch (error) {
+    console.error(error);
+    throw Error(error);
+    // return '성공';
+  }
+};
+
+export const fetchWishContent = async (fetchWishContentRequestDTO: FetchWishContentRequestDTO) => {
+  try {
+    const result = AxiosConfig.get(
+      `content/wish/${fetchWishContentRequestDTO.userId}/${fetchWishContentRequestDTO.contentId}`,
+    );
+    return result;
+  } catch (error) {
+    console.error(error);
+    throw Error(error);
+  }
+};
+
+export const fetchWatchContent = async (
+  fetchWatchContentRequestDTO: FetchWatchContentRequestDTO,
+) => {
+  try {
+    const result = AxiosConfig.get(
+      `content/watch/${fetchWatchContentRequestDTO.userId}/${fetchWatchContentRequestDTO.contentId}`,
+    );
+    return result;
+  } catch (error) {
+    console.error(error);
+    throw Error(error);
+  }
+};
+
+export const fetchLikeReview = async (fetchLikeReviewRequestDTO: FetchLikeReviewRequestDTO) => {
+  //true or false
+  try {
+    const result = await AxiosConfig.get(
+      `review/${fetchLikeReviewRequestDTO.userId}/${fetchLikeReviewRequestDTO.reviewId}`,
+    );
+    return result;
+  } catch (error) {
+    throw Error(error);
+  }
+};
+
+export const sendLikeReview = async (sendLikeReviewRequestDTO: SendLikeReviewRequestDTO) => {
+  try {
+    const result = await AxiosConfig.post(`review/like`, sendLikeReviewRequestDTO);
+    return result;
+  } catch (error) {
+    throw Error(error);
+  }
+};
+
+export const sendDislikeReview = async (
+  sendDislikeReviewRequestDTO: SendDislikeReviewRequestDTO,
+) => {
+  try {
+    const result = await AxiosConfig.post(`review/dislike`, sendDislikeReviewRequestDTO);
+    return result;
+  } catch (error) {
+    throw Error(error);
+  }
+};
+
+export const getUserInfo = async (userId: string) => {
+  try {
+    const result = await AxiosConfig.get(`user/profile/${userId}`);
+    return result;
+  } catch (error) {
+    throw Error(error);
+  }
 };
