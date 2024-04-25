ab2537f3cf217295b5a9a464e4466a0b2d6d5c2c
강경규 <qwzx16@naver.com>
2024-04-25 19:41:01 +0900

    FEAT: recoil -> redux

--- src/shared/hooks/auth/useLogin.ts
+++ src/shared/hooks/auth/useLogin.ts
@@ -1,28 +1,29 @@
 import { useNavigate } from 'react-router-dom';
 import { useMutation } from '@tanstack/react-query';
-import { useSetRecoilState } from 'recoil';
-import { isLoggedInState, userInfoState } from '../../../app/store/atoms';
 import * as API from '../../api/API';
+import { useAppDispatch } from '../../../app/store';
+import { setIsLoggedIn, setUserInfo } from '../../../app/store/slice/userSlice';
 
 const useLogin = () => {
   const navigator = useNavigate();
-  const setUserInfo = useSetRecoilState(userInfoState);
-  const setIsLoggedIn = useSetRecoilState(isLoggedInState);
+  const dispatch = useAppDispatch();
 
   return useMutation({
     mutationKey: ['login'],
     mutationFn: API.submitLogin,
 
     onSuccess: (result) => {
-      console.log(result);
       const userInfo = result.data.userInfo._doc;
       const isLoggedIn = result.data.isLoggedIn;
-      setUserInfo(userInfo);
-      setIsLoggedIn(isLoggedIn);
+      dispatch(setIsLoggedIn(isLoggedIn));
+      dispatch(setUserInfo(userInfo));
       navigator('/');
     },
     onError: (error) => {
       throw error;
+    },
+    onSettled: (error) => {
+      console.log(error);
     },
   });
 };
