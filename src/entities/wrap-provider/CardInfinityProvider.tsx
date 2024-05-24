b0d23f69497e5882c0b3ce75402a051b6c11e5dd
강경규 <qwzx16@naver.com>
2024-05-25 00:53:52 +0900

    feat: 유명 콘텐츠 카드 리스트 무한스크롤 작업 완료

--- src/entities/wrap-provider/CardInfinityProvider.tsx
+++ src/entities/wrap-provider/CardInfinityProvider.tsx
@@ -46,8 +46,26 @@ const CardInfinityProvider: React.FC<CardInfinityProviderProps> = ({ title, ...p
               </React.Fragment>
             );
           })}
-          <div className='w-12 h-full' ref={observeTargetRef}>
-            무한!
+          <div
+            className='flex items-center justify-center md:w-[card-img-w] w-[card-img-sm-w] px-10'
+            ref={observeTargetRef}
+          >
+            <Loading />
+            {/* <div className='border border-blue-300 shadow rounded-md p-4 w-full h-full'>
+              <div className='animate-pulse flex space-x-4'>
+                <div className='rounded-full bg-slate-700 h-10 w-full'></div>
+                <div className='flex-1 space-y-6 py-1'>
+                  <div className='h-10 bg-slate-700 rounded'></div>
+                  <div className='space-y-3'>
+                    <div className='grid grid-cols-3 gap-4'>
+                      <div className='h-8 bg-slate-700 rounded col-span-2'></div>
+                      <div className='h-8 bg-slate-700 rounded col-span-1'></div>
+                    </div>
+                    <div className='h-8 bg-slate-700 rounded'></div>
+                  </div>
+                </div>
+              </div>
+            </div> */}
           </div>
         </article>
       </section>
--- src/entities/wrap-provider/CardWrapProvider.tsx
+++ src/entities/wrap-provider/CardWrapProvider.tsx
@@ -4,12 +4,10 @@ import Loading from '../../shared/ui/Loading';
 import { cn } from '../../shared/lib/cn';
 import { TCardContent } from '../../app/types/main';
 import CardWrapButtons from './CardWrapButtons';
-// import axios from 'axios';
 
 type CardWrapProviderProps = {
   title: string;
   cardList: TCardContent[];
-  isHover?: boolean;
   observeTargetRef?: React.Ref<HTMLDivElement>; // ref 타입 지정
 };
 
@@ -29,13 +27,16 @@ const CardWrapProvider: React.FC<CardWrapProviderProps> = ({
     <section className={cn(`w-full relative z-10 py-3 md:py-5`)} {...props}>
       <CardWrapButtons cardContainerRef={cardContainerRef} />
       <p className='md:text-3xl text-lg'>{title}</p>
+
       <article
         ref={cardContainerRef}
         className='flex overflow-x-scroll overflow-y-hidden gap-5 py-5 smooth-scroll'
       >
         {cardList.map((content, index) => {
           const isLastCard = index === cardList.length - 1;
-          return <Card ref={isLastCard ? observeTargetRef : null} key={index} content={content} />;
+          return (
+            <Card ref={isLastCard ? observeTargetRef : null} key={content._id} content={content} />
+          );
         })}
       </article>
     </section>
