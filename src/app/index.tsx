1afa0854a8527ba03d770b26e64d129c011f5ea0
강경규 <qwzx16@naver.com>
2024-05-28 22:12:41 +0900

    REFACTOR: app-provider 생성하여 별도 관리

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -6,15 +6,16 @@ import DarkModeButton from './dark-mode-button';
 import Routing from '../pages';
 import ErrorProvider from './error-provider';
 import Fullback from './full-back';
-import { cn } from '../shared/lib/cn';
-import { useAppSelector } from './store';
-import { ErrorBoundary } from 'react-error-boundary';
 import DetailBackground from '../pages/ui/DetailBackground';
+import { useAppSelector } from './store';
+import { AppProvider } from './app-provider';
+import { ErrorBoundary } from 'react-error-boundary';
+
 function App() {
-  const { darkModeClasses } = useAppSelector((state) => state.darkMode);
   const { backgroundPath } = useAppSelector((state) => state.backgroundPath);
+
   return (
-    <main className={cn(`${darkModeClasses} transition-all`)}>
+    <AppProvider>
       <Header />
       <DetailBackground path={backgroundPath} />
       <ErrorBoundary fallback={<ErrorProvider />}>
@@ -22,9 +23,9 @@ function App() {
           <Routing />
         </Suspense>
       </ErrorBoundary>
-      <Footer />
       <DarkModeButton />
-    </main>
+      <Footer />
+    </AppProvider>
   );
 }
 
