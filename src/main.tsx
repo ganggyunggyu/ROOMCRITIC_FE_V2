c87e746a6610cce96c9fd5eb13e2d4e06a845ae8
강경규 <qwzx16@naver.com>
2024-06-04 14:07:06 +0900

    chore: recoil provider delete

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,8 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './app/index.tsx';
 import './app/styles/index.css';
 
 import { BrowserRouter } from 'react-router-dom';
-import { RecoilRoot } from 'recoil';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { Provider } from 'react-redux';
@@ -17,13 +15,10 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
-        <RecoilRoot>
-          <Interceptor>
-            <ReactQueryDevtools />
-            <App />
-          </Interceptor>
-          ,
-        </RecoilRoot>
+        <Interceptor>
+          <ReactQueryDevtools />
+          <App />
+        </Interceptor>
       </BrowserRouter>
     </QueryClientProvider>
   </Provider>,
