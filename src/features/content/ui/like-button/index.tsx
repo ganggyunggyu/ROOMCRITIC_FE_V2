c3ba81576eec2e1c1929531c520b28e1cab17297
ganggyunggyu <qwzx16@naver.com>
2024-12-06 20:30:35 +0900

    style: update icon

--- src/features/content/ui/like-button/index.tsx
+++ src/features/content/ui/like-button/index.tsx
@@ -1,9 +1,9 @@
-import { FaRegThumbsUp } from 'react-icons/fa';
+import { FaThumbsUp } from 'react-icons/fa';
 
 export const LikeButton = () => {
   return (
     <button className="w-24 h-24 flex flex-col items-center justify-center gap-2">
-      <FaRegThumbsUp className="w-full h-full" />
+      <FaThumbsUp className="w-full h-full" />
       <p>좋아요!</p>
     </button>
   );
