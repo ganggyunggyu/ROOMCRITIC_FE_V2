24d49b5c24d4e6b990656282affe4aa2a857e7ce
ganggyunggyu <qwzx16@naver.com>
2024-12-24 14:33:32 +0900

    feat: button click ui event

--- src/features/content/ui/watch-button/index.tsx
+++ src/features/content/ui/watch-button/index.tsx
@@ -1,9 +1,15 @@
+import React from 'react';
 import { FaEye } from 'react-icons/fa';
 
 export const WatchButton = () => {
+  const [islike, setIslike] = React.useState(false);
+
+  const handleLikeClick = () => {
+    setIslike(!islike);
+  };
   return (
-    <button className="w-24 h-24 flex flex-col items-center justify-center gap-2">
-      <FaEye className="w-full h-full" />
+    <button onClick={handleLikeClick} className="w-24 h-24 flex flex-col items-center justify-center gap-2">
+      <FaEye className={`w-full h-full ${islike && 'fill-violet-500'}`} />
       <p>보는중!</p>
     </button>
   );
