be0524bde003a8907e64cf4a38ff7107e9106d42
ganggyunggyu <qwzx16@naver.com>
2025-01-17 22:06:59 +0900

    fix: 검색 콘텐츠 없을 시 분기 추가

--- src/features/content/ui/category-content-list/index.tsx
+++ src/features/content/ui/category-content-list/index.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
-import { useAppSelector } from '../../../../app/store';
-import { Content, useLatestContentQuery, usePopularContentQuery } from '../../../../entities';
+import { useAppSelector } from '@/app/store';
+import {
+  useLatestContentQuery,
+  usePopularContentQuery,
+  useRecentlyCreateReviewContent,
+} from '@/entities';
 import { ContentInfinityList } from '../content-infinity-list';
 import { ContentList } from '../content-list';
 
@@ -9,9 +13,18 @@ export const Category = () => {
 
   return (
     <React.Fragment>
-      {searchContents && <ContentList title={'최근 검색 결과'} cardList={searchContents} />}
+      {searchContents?.length !== 0 && (
+        <ContentList title={'최근 검색 결과'} cardList={searchContents} />
+      )}
+      <ContentInfinityList
+        title="최근에 리뷰가 작성된 작품들"
+        query={useRecentlyCreateReviewContent}
+      />
       <ContentInfinityList title="명작 모음" query={usePopularContentQuery} />
-      <ContentInfinityList title="개봉 예정 작품" query={useLatestContentQuery} />
+      <ContentInfinityList
+        title="개봉 예정 작품들"
+        query={useLatestContentQuery}
+      />
     </React.Fragment>
   );
 };
