d2c9003cfc1224999f57c6892ed3c24de5d69693
ganggyunggyu <qwzx16@naver.com>
2025-01-19 13:21:26 +0900

    chore: test info env

--- src/config/env-config.ts
+++ src/config/env-config.ts
@@ -1,5 +1,10 @@
 const isDev = import.meta.env.DEV;
 
-export const HOST = isDev ? import.meta.env.VITE_API_HOST : import.meta.env.VITE_API_HOST;
+export const HOST = isDev
+  ? import.meta.env.VITE_API_HOST
+  : import.meta.env.VITE_API_HOST;
 
 export const TMDB_TOKEN = import.meta.env.VITE_TMDB_TOKEN;
+
+export const TEST_EMAIL = import.meta.env.VITE_TEST_EMAIL;
+export const TEST_PASSWORD = import.meta.env.VITE_TEST_PASSWORD;
--- src/test/auth/auth.test.ts
+++ src/test/auth/auth.test.ts
@@ -1,10 +1,11 @@
+import { TEST_EMAIL, TEST_PASSWORD } from '@/config/env-config';
 import { LoginRequest, JoinRequest, submitJoin, submitLogin } from '@/entities';
 
 import { beforeAll, describe, expect, it, test } from 'vitest';
 
 const LoginMock = {
-  email: 'qwzx16@naver.com',
-  password: '12Qwaszx!@',
+  email: TEST_EMAIL,
+  password: TEST_PASSWORD,
 } as LoginRequest;
 
 test('Login', async () => {
