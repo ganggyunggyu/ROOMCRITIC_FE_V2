008255cbd3e25a0cba7606f01a0904c68a25e3f3
ganggyunggyu <qwzx16@naver.com>
2025-01-19 13:18:58 +0900

    chore: 주석 제거

--- src/entities/content/hooks/content.hooks.ts
+++ src/entities/content/hooks/content.hooks.ts
@@ -15,44 +15,12 @@ import { setSearchContents } from '@/app/store/slice/searchSlice';
 export const useAddWatchContent = () => {
   return useMutation({
     mutationFn: addWatchContent,
-    // onSuccess(data, variables, context) {
-    //   console.log('Success', data);
-    //   console.log('Success', variables);
-    //   console.log('Success', context);
-    // },
-    // onError(error, variables, context) {
-    //   console.log('Error', error);
-    //   console.log('Error', variables);
-    //   console.log('Error', context);
-    // },
-    // onSettled(data, error, variables, context) {
-    //   console.log('Settled', data);
-    //   console.log('Settled', error);
-    //   console.log('Settled', variables);
-    //   console.log('Settled', context);
-    // },
   });
 };
 
 export const useAddWishContent = () => {
   return useMutation({
     mutationFn: addWishContent,
-    // onSuccess(data, variables, context) {
-    //   console.log('Success', data);
-    //   console.log('Success', variables);
-    //   console.log('Success', context);
-    // },
-    // onError(error, variables, context) {
-    //   console.log('Error', error);
-    //   console.log('Error', variables);
-    //   console.log('Error', context);
-    // },
-    // onSettled(data, error, variables, context) {
-    //   console.log('Settled', data);
-    //   console.log('Settled', error);
-    //   console.log('Settled', variables);
-    //   console.log('Settled', context);
-    // },
   });
 };
 
@@ -157,7 +125,6 @@ export const useRecentlyCreateReviewContent = (contentType?: string) => {
     queryFn: ({ pageParam }) => getRecentlyCreateReviewContent(pageParam, t),
     initialPageParam: 0,
     getNextPageParam: (lastPage, pages, allPages) => {
-      console.log(allPages);
       return allPages + 10;
     },
     select: (data) => data.pages,
