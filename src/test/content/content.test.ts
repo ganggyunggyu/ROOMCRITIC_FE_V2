f44a0baf8731a57f57822992206f24d350960cc4
ganggyunggyu <qwzx16@naver.com>
2025-01-26 19:08:01 +0900

    feat: contnet-list get api test

--- src/test/content/content.test.ts
+++ src/test/content/content.test.ts
@@ -1,8 +1,16 @@
-import { getRecentlyCreateReviewContent } from '@/entities';
 import { test } from 'vitest';
+import {
+  getRecentlyReviewedContent,
+  RecentlyReviewedContentRequest,
+} from '@/entities';
 
 test('최근에 작성 된 리뷰가 있는 콘텐츠 리스트 데이터를 가져옵니다.', async () => {
-  const result = await getRecentlyCreateReviewContent(0);
+  const MOCK_REQUEST: RecentlyReviewedContentRequest = {
+    pageParam: 0,
+    contentType: null,
+  };
 
-  console.log(result.length);
+  const result = await getRecentlyReviewedContent(MOCK_REQUEST);
+
+  result.data.contentList.map((content) => console.log(content.title));
 });
