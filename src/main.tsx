5f592108748ea5280a7a0ce26cd201f24fff8145
ganggyunggyu <qwzx16@naver.com>
2025-05-30 14:43:45 +0900

    chore: root tsx에선 라이브러리 등만 관리

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import App from './app/index.tsx';
+import { App } from './app/index.tsx';
 import './app/styles/index.css';
 
 import { BrowserRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { Provider } from 'react-redux';
-import { Interceptor } from './app/axios-interceptor';
+
 import { store } from './app/store/index.ts';
 
 const queryClient = new QueryClient();
@@ -18,10 +18,8 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <QueryClientProvider client={queryClient}>
       <React.StrictMode>
         <BrowserRouter>
-          <Interceptor>
-            <ReactQueryDevtools />
-            <App />
-          </Interceptor>
+          <ReactQueryDevtools />
+          <App />
         </BrowserRouter>
       </React.StrictMode>
     </QueryClientProvider>
